01e07c04ed79140fbd480bdb338a7a2573fcffe5
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-05-24 12:41:01 +0300

    3. Состояние нити. [#229175 #497446]

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,19 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> { }
+        );
+        Thread second = new Thread(
+                () -> { }
+        );
+        System.out.println(first.getState());
+        first.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName());
+            System.out.println(second.getName());
+        }
+        System.out.println("Done");
+    }
+}
