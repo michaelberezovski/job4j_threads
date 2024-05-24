0e3af75097e012afacd435b555d2c7877f776751
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-05-24 15:54:33 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                for (char c : process) {
+                    System.out.print("\r load: " + c);
+                    Thread.sleep(500);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
