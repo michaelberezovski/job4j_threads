22983fffd738b27c148701d7867ad43f2e641fc0
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-06-11 11:25:52 +0300

    8. Скачивание файла с ограничением. [#144271 #497733]

--- src/main/java/thread/Wget.java
+++ src/main/java/thread/Wget.java
@@ -1,22 +1,53 @@
 package thread;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileForDownload;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileForDownload) {
         this.url = url;
         this.speed = speed;
+        this.fileForDownload = fileForDownload;
     }
 
     @Override
     public void run() {
+        var startAt = System.currentTimeMillis();
+        var file = new File("tmp.xml");
+        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        try (var input = new URL(url).openStream();
+             var output = new FileOutputStream(file)) {
+            System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
+            var dataBuffer = new byte[1024];
+            int bytesRead;
+            double time;
+            double speed;
+            while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                var downloadAt = System.nanoTime();
+                output.write(dataBuffer, 0, bytesRead);
+                time = System.nanoTime() - downloadAt;
+                speed = (dataBuffer.length / time) * 1000000;
+                if (speed > 1000) {
+                    long sleepTime = (long) speed / 1000;
+                    Thread.sleep(sleepTime);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createFile(String name) {
+        File file = new File("src/main/java/thread/" + name);
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -36,9 +67,10 @@ public class Wget implements Runnable {
         }
         int speed = Integer.parseInt(args[1]);
         if (speed > 1000) {
-            throw new IllegalArgumentException("Speed must be lower than a 1000.");
+            throw new IllegalArgumentException("Speed limit must be lower than a 1000.");
         }
-        Thread wget = new Thread(new Wget(urlStr, speed));
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(urlStr, speed, fileName));
         wget.start();
         wget.join();
     }
