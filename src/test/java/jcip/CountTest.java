02aa0482850e2ec9d66a29f0408261a6c95de750
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-06-22 19:37:58 +0300

    2. JCIP. Настройка библиотеки [#268575]

---
+++ src/test/java/jcip/CountTest.java
@@ -0,0 +1,19 @@
+package jcip;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CountTest {
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        var count = new Count();
+        var first = new Thread(count::increment);
+        var second = new Thread(count::increment);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get()).isEqualTo(2);
+    }
+}
