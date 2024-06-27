14b1921c097d76d850da9e1541361c32919a7b35
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-06-27 14:32:57 +0300

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/pool/ThreadPool.java
@@ -0,0 +1,39 @@
+package pool;
+
+import blockingqueue.SimpleBlockingQueue;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final int size = Runtime.getRuntime().availableProcessors();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
+
+    public ThreadPool() {
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(
+                    () -> {
+                        try {
+                            while (!Thread.currentThread().isInterrupted()) {
+                                tasks.poll();
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            );
+            thread.start();
+            threads.add(thread);
+        }
+    }
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+}
