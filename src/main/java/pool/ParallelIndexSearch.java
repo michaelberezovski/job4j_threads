6f23a6871dc0f9e866dab34ead47e55c2731cfe6
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-07-04 12:22:23 +0300

    3. ForkJoinPool [#315067]

--- src/main/java/pool/ParallelIndexSearch.java
+++ src/main/java/pool/ParallelIndexSearch.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
-
     private final T[] array;
     private final T elem;
     private final int from;
@@ -17,9 +16,9 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
         this.to = to;
     }
 
-    private int linearSearch(T[] tArray, T element) {
-        for (int i = 0; i < tArray.length; i++) {
-            if (tArray[i] == element) {
+    private int linearSearch() {
+        for (int i = from; i <= to; i++) {
+            if (elem.equals(array[i])) {
                 return i;
             }
         }
@@ -28,17 +27,21 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
 
     @Override
     protected Integer compute() {
-        if (array.length <= 10) {
-            return linearSearch(array, elem);
+        if (to - from + 1 <= 10) {
+            return linearSearch();
         }
         int middle = (from + to) / 2;
-        ParallelIndexSearch leftSort = new ParallelIndexSearch(array, elem, from, middle);
-        ParallelIndexSearch rightSort = new ParallelIndexSearch(array, elem, middle + 1, to)
+        ParallelIndexSearch<T> leftSearch = new ParallelIndexSearch<>(array, elem, from, middle);
+        ParallelIndexSearch<T> rightSearch = new ParallelIndexSearch<>(array, elem, middle + 1, to);
+        leftSearch.fork();
+        rightSearch.fork();
+        int leftIndex = leftSearch.join();
+        int rightIndex = rightSearch.join();
         return null;
     }
 
-    public int[] sort(int[] array) {
+    public static <T> int search(T[] array, T elem) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(new ParallelIndexSearch(array, elem, 0, array.length - 1));
+        return forkJoinPool.invoke(new ParallelIndexSearch<>(array, elem, 0, array.length - 1));
     }
 }
